c834081c1156d516b7d3865bd90642e87103596c
Stephàn Costa <stephan.acosta1@outlook.com>
2023-10-28 16:45:50 -0300

    magicalbookstore

---
+++ src/components/bookdetails.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface BookDetailsProps{
+    book: Book;
+    onClose: () => void;
+}
+
+const BookDetails: React.FC<BookDetailsProps> = ({book, onClose}) => {
+    return(
+        <div className="book-details">
+            <h2>Detalhes do Livro</h2>
+            <p>Título: {book.title}</p>
+            <p>Autor: {book.author}</p>
+            <p>Ano de Publicação: {book.publicationYear}</p>
+            <p>Data de Cadastro: {book.dateAdded}</p>
+            <p>Gênero: {book.genre}</p>
+            <p>Descrição: {book.description}</p>
+            <button onClick={onClose}>Fechar</button>
+        </div>
+    );
+};
+
+export default BookDetails;
