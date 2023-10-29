e88d45f7e0cbafcf28be3e0ddc65dfb7d16d9d6a
Stephàn Costa <stephan.acosta1@outlook.com>
2023-10-29 09:07:30 -0300

    magicalbookstore

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
+import {BookList} from './components/booklist';
+import {BookDetails} from './components/bookdetails';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [newBook, setNewBook] = useState<Book>({
+    id: 0,
+    title: "",
+    author: "",
+    publicationYear: 0,
+    dateAdded: "",
+    genre: "",
+    description: "",
+  });
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [editingBook, setEditingBook] = useState<Book | null>(null);
+
+  const handleAddBook = () => {
+    if (newBook.title && newBook.author && newBook.publicationYear) {
+      if (newBook.publicationYear <= new Date().getFullYear()) {
+        const updatedBooks = [...books];
+        newBook.id = books.length + 1;
+        newBook.dateAdded = new Date().toLocaleDateString();
+        updatedBooks.push(newBook);
+        setBooks(updatedBooks);
+        setNewBook({
+          id: 0,
+          title: "",
+          author: "",
+          publicationYear: 0,
+          dateAdded: "",
+          genre: "",
+          description: "",
+        });
+      } else {
+        alert("O ano de publicação não pode ser no futuro.");
+      }
+    } else {
+      alert("Todos os campos são obrigatórios.");
+    }
+  };
+
+
+  const handleDeleteBook = (id: number) => {
+    if (window.confirm("Tem certeza de que deseja excluir este livro?")) {
+      const updatedBooks = books.filter((book) => book.id !== id);
+      setBooks(updatedBooks);
+    }
+  };
 
   return (
-    <>
+    <div>
+      <h1>Minha Coleção de Livros</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Adicionar Novo Livro</h2>
+        <input
+          type="text"
+          placeholder="Título"
+          value={newBook.title}
+          onChange={(e) => setNewBook({ ...newBook, title: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Autor"
+          value={newBook.author}
+          onChange={(e) => setNewBook({ ...newBook, author: e.target.value })}
+        />
+        <input
+          type="number"
+          placeholder="Ano de Publicação"
+          value={newBook.publicationYear}
+          onChange={(e) =>
+            setNewBook({ ...newBook, publicationYear: parseInt(e.target.value) })
+          }
+        />
+        <button onClick={handleAddBook}>Adicionar</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <h2>Lista de Livros</h2>
+        <ul>
+          {books.map((book) => (
+            <li key={book.id}>
+              {book.title} - {book.author} ({book.publicationYear})
+              <button onClick={() => handleDeleteBook(book.id)}>Excluir</button>
+            </li>
+          ))}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/booklist.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface BookListProps {
+  books: Book[];
+  onEdit: (book: Book) => void;
+  onDelete: (id: number) => void;
+  onViewDetails: (book: Book) => void;
+}
+
+const BookList: React.FC<BookListProps> = ({ books, onEdit, onDelete, onViewDetails }) => {
+  return (
+    <ul>
+      {books.map((book) => (
+        <li key={book.id}>
+          {book.title} - {book.author} ({book.publicationYear})
+          <button onClick={() => onEdit(book)}>Editar</button>
+          <button onClick={() => onDelete(book.id)}>Excluir</button>
+          <button onClick={() => onViewDetails(book)}>Ver Detalhes</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default BookList;
